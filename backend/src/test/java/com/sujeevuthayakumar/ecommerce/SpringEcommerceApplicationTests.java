f8594f1642d2840d1ae9af2b77032bf201cb503f
Sujeev-Uthayakumar <sujeev115115@gmail.com>
2022-08-10 00:23:23 -0400

    add init backend

---
+++ backend/src/test/java/com/sujeevuthayakumar/ecommerce/SpringEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sujeevuthayakumar.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
