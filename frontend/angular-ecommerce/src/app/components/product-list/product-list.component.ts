9f1534f1fd13d84c292deebb947d76425c711cbd
Sujeev-Uthayakumar <sujeev115115@gmail.com>
2022-08-31 10:27:54 -0400

    refactor

--- frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -72,12 +72,7 @@ export class ProductListComponent implements OnInit {
         this.thePageSize,
         this.currentCategoryId
       )
-      .subscribe((data) => {
-        this.products = data._embedded.products;
-        this.thePageNumber = data.page.number + 1;
-        this.thePageSize = data.page.size;
-        this.theTotalElements = data.page.totalElements;
-      });
+      .subscribe(this.processResult());
   }
 
   handleSearchProducts() {
