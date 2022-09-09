a01ae90b4174cf8ad57b6d8163169a1d151d7dd2
Sujeev-Uthayakumar <sujeev115115@gmail.com>
2022-09-08 23:48:52 -0400

    add decrementQuantity method

--- frontend/angular-ecommerce/src/app/components/cart-details/cart-details.component.ts
+++ frontend/angular-ecommerce/src/app/components/cart-details/cart-details.component.ts
@@ -30,4 +30,12 @@ export class CartDetailsComponent implements OnInit {
     // compute cart total price and quantity
     this.cartService.computeCartTotals();
   }
+
+  incrementQuantity(theCartItem: CartItem) {
+    this.cartService.addToCart(theCartItem);
+  }
+
+  decrementQuantity(theCartItem: CartItem) {
+    this.cartService.decrementQuantity(theCartItem);
+  }
 }
