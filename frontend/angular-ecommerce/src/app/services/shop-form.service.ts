7ef76a7832d19c21afedd6df7296bfb8668979fa
Sujeev-Uthayakumar <sujeev115115@gmail.com>
2022-09-17 23:23:01 -0400

    rename shop-form service

--- frontend/angular-ecommerce/src/app/services/shop-form.service.ts
+++ frontend/angular-ecommerce/src/app/services/shop-form.service.ts
@@ -4,7 +4,7 @@ import { Observable, of } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
-export class ShopFormServiceService {
+export class ShopFormService {
   constructor() {}
 
   getCreditCardMonths(startMonth: number): Observable<number[]> {
