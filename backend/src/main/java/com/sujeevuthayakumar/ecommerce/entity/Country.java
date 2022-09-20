6c9858295b5c44302f7928c7b4d2b5045a3ceb01
Sujeev-Uthayakumar <sujeev115115@gmail.com>
2022-09-19 21:22:44 -0400

    add Country entity

---
+++ backend/src/main/java/com/sujeevuthayakumar/ecommerce/entity/Country.java
@@ -0,0 +1,27 @@
+package com.sujeevuthayakumar.ecommerce.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="country")
+@Getter
+@Setter
+public class Country {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "code")
+    private String code;
+
+    @Column(name = "name")
+    private String name;
+
+    // TODO: Set up one-to-many mapping with states
+
+}
