3d61080bb1b5e1b36ee68d71fd0b951fbdf91bfc
Sujeev-Uthayakumar <sujeev115115@gmail.com>
2022-12-02 01:17:50 -0500

    add way to view customer orders

--- backend/src/main/java/com/sujeevuthayakumar/ecommerce/config/MyDataRestConfig.java
+++ backend/src/main/java/com/sujeevuthayakumar/ecommerce/config/MyDataRestConfig.java
@@ -1,9 +1,6 @@
 package com.sujeevuthayakumar.ecommerce.config;
 
-import com.sujeevuthayakumar.ecommerce.entity.Country;
-import com.sujeevuthayakumar.ecommerce.entity.Product;
-import com.sujeevuthayakumar.ecommerce.entity.ProductCategory;
-import com.sujeevuthayakumar.ecommerce.entity.State;
+import com.sujeevuthayakumar.ecommerce.entity.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
@@ -42,6 +39,7 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
         disableHttpMethods(ProductCategory.class, config, theUnsupportedActions);
         disableHttpMethods(Country.class, config, theUnsupportedActions);
         disableHttpMethods(State.class, config, theUnsupportedActions);
+        disableHttpMethods(Order.class, config, theUnsupportedActions);
 
         // call internal helper method
         exposeIds(config);
